af43781073bc066d05b597346a2910596966286f
Clacky <develop@clacky.ai>
2025-01-16 04:07:59 -0500

    update: database credentials and Redis password in settings.py

--- chat/settings.py
+++ chat/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'chat',
-        'USER': 'root_xFqhREJQwKTWHysWJdpY',
-        'PASSWORD': 'EFnJGVOk',
+        'USER': 'root_mZPTRmNQJLqfCGEXKcTR',
+        'PASSWORD': 'ZDYXlouW',
         'HOST': 'mysql.inner.clackypaas.com',
         'PORT': '3306',
         'OPTIONS': {
@@ -185,7 +185,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             'hosts': ['redis://:{password}@{host}:{port}/0'.format(
-                password='r-VEDsTEtexJlpEGBLplYz',
+                password='r-jzUrcJSvsPyRueHeZZuQ',
                 host='redis.inner.clackypaas.com',
                 port=6379
             )]
